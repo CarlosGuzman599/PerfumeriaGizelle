e6ac1a9922f8f5af6fce8293d0e4288574de5af1
CarlosGuzman599 <carlilloz.599@gmail.com>
2021-03-18 20:37:11 -0600

    LimpioPC, trabajo reajustando para carrito en productos

--- includes/templates/funciones/listado_productos.php
+++ includes/templates/funciones/listado_productos.php
@@ -1,6 +1,6 @@
 <?php 
 
-  $base = "SELECT `nombre_producto`, `precio_producto`, `categoria_producto`, `descri_producto`, `img_producto`, `icono_categoria` FROM `productos` INNER JOIN `categoria` ON productos.categoria_producto = categoria.id_categoria";
+  $base = "SELECT `id_producto`, `nombre_producto`, `precio_producto`, `categoria_producto`, `descri_producto`, `img_producto`, `icono_categoria` FROM `productos` INNER JOIN `categoria` ON productos.categoria_producto = categoria.id_categoria";
 
   if($_GET['tipo'] == 'todo'){
     $sql = $base;
@@ -28,17 +28,21 @@
   
 ?>
 
-<div id="contenido-muestra" class="contenido-muestra clearfix">
+<div id="contenido-muestra" class="contenido-muestra clearfix contenedor">
 
-  <ul class="lista-productos productos">
+  <ul class="catalogo">
 
     <?php while($producto = $resultado->fetch_assoc()){ ?>
 
       <li>
         <img class="img-producto" src="<?php echo $producto['img_producto'] ?>" alt="producto">
         <p class="nombre-p"><?php echo $producto['nombre_producto'] ?></p>
-        <p class="precio-p"><img class="icono-categoria" src="<?php echo $producto['icono_categoria'] ?>" alt=""><span>$</span><?php echo $producto['precio_producto'] ?></p>
-        <a href="#" class="boton">+<i class="fas fa-shopping-cart"></i></a>
+        <p class="categoria-precio">
+          <img class="icono-categoria" src="<?php echo $producto['icono_categoria'] ?>" alt="">
+          <span>$</span><?php echo $producto['precio_producto'] ?>
+        </p>
+        <p class="btn-addcar" id="<?php echo $producto['id_producto'] ?>"><i class="fas fa-cart-plus"></i></p>
+        <!-- a href="#" class="boton">+<i class="fas fa-shopping-cart"></i></a> -->
       </li>
 
     <?php } 
